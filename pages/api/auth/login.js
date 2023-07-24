5275cc5a08e598b1d1b4807cfad47012f3c8c8d9
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-23 19:54:40 -0600

    send token in cookie

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,3 +1,26 @@
-export default function loginHandler(req, res){
-    return res.json('login route')
+import jwt from "jsonwebtoken";
+import { serialize } from "cookie";
+
+export default function loginHandler(req, res) {
+  const { email, password } = req.body;
+  if (email === "admin@local.local" && password === "admin") {
+    const token = jwt.sign(
+      {
+        exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30, // 30 days
+        email: "admin@local.local",
+        password: "anibal",
+      },
+      "secret"
+    );
+    const serialized = serialize("myTokenName", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+      path: "/",
+    });
+    res.setHeader("Set-Cookie", serialized);
+    return res.json("login success");
+  }
+  return res.status(401).json({ message: "Login failed" });
 }
