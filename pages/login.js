db0483d29ed741af3d0fc05523af118e6c6fcb82
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-07-23 19:35:31 -0600

    backend setup

---
+++ pages/login.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import axios from "axios";
+
+function login() {
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(credentials);
+    const response = await axios.post("/api/auth/login", credentials);
+    console.log(response);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default login;
